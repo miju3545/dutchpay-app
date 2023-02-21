d6035ada25a22ad2d6d7607531ada7624b45ff6d
rovxxmjxx <miju3545@gmail.com>
2023-02-22 00:46:19 +0900

    feat: createGroup, addMembers + customHooks

---
+++ src/components/common/GroupLayout/GroupLayout.tsx
@@ -0,0 +1,26 @@
+import { ComponentType, FC, ReactNode } from 'react';
+import { GroupProvider } from '../../group/context';
+
+const Layout: FC<{ children: ReactNode }> = ({ children }) => {
+  return (
+    <GroupProvider>
+      <div style={{ minWidth: '500px', border: '1px solid #dfdfdf' }}>
+        <div>{children}</div>
+      </div>
+    </GroupProvider>
+  );
+};
+
+type Props = {};
+
+const GroupLayout = <T extends Props>(Wrapped: ComponentType<T>) => {
+  return (props: T) => {
+    return (
+      <Layout>
+        <Wrapped {...props} />
+      </Layout>
+    );
+  };
+};
+
+export default GroupLayout;
