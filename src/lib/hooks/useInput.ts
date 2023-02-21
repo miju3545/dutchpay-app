3152e374c9a9c0c6f0074f6dafd6e1e9c18604f2
rovxxmjxx <miju3545@gmail.com>
2023-02-21 17:43:35 +0900

    feat: useInput

---
+++ src/lib/hooks/useInput.ts
@@ -0,0 +1,35 @@
+import { useState, ChangeEvent, useRef, MutableRefObject, useEffect } from 'react';
+
+const useInput = (
+  initialValue: string
+): {
+  ref: MutableRefObject<HTMLInputElement>;
+  value: string;
+  isDirty: boolean;
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  onReset: () => void;
+  onFocus: () => void;
+} => {
+  const ref = useRef() as MutableRefObject<HTMLInputElement>;
+  const [value, setValue] = useState(initialValue);
+  const [isDirty, setDirty] = useState(false);
+
+  const handler = (e: ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value);
+  };
+
+  const onReset = () => setValue(initialValue);
+
+  const onFocus = () => {
+    if (ref.current) {
+      ref.current.focus();
+    }
+  };
+
+  useEffect(() => {
+    setDirty(!!value.length);
+  }, [value]);
+  return { ref, value, isDirty, onChange: handler, onReset, onFocus };
+};
+
+export default useInput;
