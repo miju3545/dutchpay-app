6488c5a38fe0c21c80f152fbb98790c8738fcc16
rovxxmjxx <miju3545@gmail.com>
2023-02-21 17:50:12 +0900

    feat: createGroup, addMembersView

--- src/components/group/AddMembersView.tsx
+++ src/components/group/AddMembersView.tsx
@@ -1,7 +1,32 @@
-import { FC } from 'react';
+import { FC, useState, SyntheticEvent } from 'react';
+import { useUI } from '../ui/context';
+import { Button } from '../ui';
 
 const AddMembersView: FC = () => {
-  return <div></div>;
+  // form state
+  const [name, setName] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [message, setMessage] = useState('');
+  const [dirty, setDirty] = useState(false);
+  const [disabled, setDisabled] = useState(false);
+  const { setModalView, closeModal } = useUI();
+
+  const handleAdd = (e: SyntheticEvent<EventTarget>) => {
+    e.preventDefault();
+
+    // setModalView('EXPENSE_VIEW');
+  };
+
+  return (
+    <div>
+      AddMembersView
+      <form onSubmit={handleAdd}>
+        <Button type="submit" loading={loading} disabled={disabled}>
+          Add!!
+        </Button>
+      </form>
+    </div>
+  );
 };
 
 export default AddMembersView;
--- src/components/group/CreateGroupView.tsx
+++ src/components/group/CreateGroupView.tsx
@@ -1,7 +1,68 @@
-import { FC } from 'react';
+import { FC, useState, SyntheticEvent, useCallback, useEffect } from 'react';
+import { useUI } from '../ui/context';
+import { Button } from '../ui';
+import Input from '../ui/Input/Input';
 
 const CreateGroupView: FC = () => {
-  return <div>CreateGroup</div>;
+  // form state
+  const [groupName, setGroupName] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [message, setMessage] = useState('');
+  const [dirty, setDirty] = useState(false);
+  const [disabled, setDisabled] = useState(true);
+  const { setModalView } = useUI();
+
+  const handleCreate = async (e: SyntheticEvent<EventTarget>) => {
+    e.preventDefault();
+
+    // if (!dirty && disabled) {
+    //   handleValidation();
+    // }
+
+    try {
+      setLoading(true);
+      setMessage('');
+      // await createGroup({ name });
+      setModalView('ADDMEMBERS_VIEW');
+    } catch ({ errors }) {
+      if (errors instanceof Array) {
+        setMessage(errors.map((e: any) => e.message).join('<br />'));
+      } else {
+        setMessage('Unexpected Error');
+      }
+      setDisabled(false);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleValidation = useCallback(() => {
+    const validName = /\w{3,}/.test(groupName);
+
+    if (dirty) {
+      setDisabled(!validName);
+    }
+  }, [groupName, dirty]);
+
+  useEffect(() => {
+    handleValidation();
+  }, [handleValidation, groupName, dirty]);
+
+  useEffect(() => {
+    if (groupName.length) setDirty(true);
+  }, [groupName]);
+
+  return (
+    <div>
+      CreateGroupView
+      <form onSubmit={handleCreate}>
+        <Input value={groupName} onChange={(e) => setGroupName(e.target.value)} />
+        <Button type="submit" loading={loading} disabled={disabled}>
+          Save Group Name.
+        </Button>
+      </form>
+    </div>
+  );
 };
 
 export default CreateGroupView;
