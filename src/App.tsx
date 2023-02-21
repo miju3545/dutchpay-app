3e328cdd3b74e0ca19a33ea03ba781679fa4ba89
rovxxmjxx <miju3545@gmail.com>
2023-02-21 17:51:16 +0900

    chore

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,26 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { CreateGroupView, AddMembersView } from './components/group';
 import { LoginView, SignUpView } from './components/auth';
 import ExpenseView from './components/expense/ExpenseView';
+import { ManagedUIContext } from './components/ui/context';
+import Layout from './components/common/Layout/Layout';
+import Intro from './pages/Intro';
 
 const App = () => {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<CreateGroupView />} />
-          <Route path="/members" element={<AddMembersView />} />
-          <Route path="/expense" element={<ExpenseView />} />
-          <Route path="/signup" element={<SignUpView />} />
-          <Route path="/login" element={<LoginView />} />
-        </Routes>
-      </BrowserRouter>
+      <ManagedUIContext>
+        <Layout>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Intro />} />
+              <Route path="/expense" element={<ExpenseView />} />
+              {/* <Route path="/members" element={<AddMembersView />} /> */}
+              {/* <Route path="/signup" element={<SignUpView />} /> */}
+              {/* <Route path="/login" element={<LoginView />} /> */}
+            </Routes>
+          </BrowserRouter>
+        </Layout>
+      </ManagedUIContext>
     </div>
   );
 };
